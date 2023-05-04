5ee75d60d7ed450bb9d8e0de65e3dcc7fc5a0050
JesusEGomez <jesus.emanuel.gomez77@gmail.com>
2023-05-04 19:57:09 -0300

    implementando filtro por peso y orden alfabetico

--- client/src/components/Filters/index.jsx
+++ client/src/components/Filters/index.jsx
@@ -1,16 +1,29 @@
 import "./index.css"
 
-const Filters = ({temperaments,filterHandler})=>{
+const Filters = ({temperaments,filterTempHandler,sortByName,sortByWeight})=>{
     return(
         <div>
             <label autoComplete="off" name="temperament">Temperamento:</label>
-                <select onChange={filterHandler}  name="temperament" >
+                <select onChange={filterTempHandler}  name="temperament" >
                 {temperaments.map((element)=>{
                     return <option key={element.id} value={element.name}>
                         {element.name}
                     </option>
                 })}
                 </select>
+
+            <label autoComplete="off" name="ordenamiento-raza">Nombres de razas</label>
+            <select onChange={sortByName} >
+                <option value=""></option>
+                <option value="descendente">descendente</option>
+                <option value="ascendente">ascendente</option>
+            </select>
+            <label autoComplete="off" name="ordenamiento-peso">Peso</label>
+            <select onChange={sortByWeight} >
+                <option value=""></option>
+                <option value="descendente">descendente</option>
+                <option value="ascendente">ascendente</option>
+            </select>
                 
         </div>
     )
--- client/src/pages/Home/index.jsx
+++ client/src/pages/Home/index.jsx
@@ -8,6 +8,8 @@ import PaginationControls from "../../components/PaginationControls"
 
 const Home = ({clickHandler})=>{
     const dispatch = useDispatch()
+    const [filteredDogs, setFilteredDogs] = useState(null)
+
     useEffect(()=>{
         dispatch(getDogs())
         dispatch(getTemperaments())
@@ -17,19 +19,87 @@ const Home = ({clickHandler})=>{
     const state = useSelector(state=>state)
     const {dogs,currentPage, itemsPerPage,temperaments} = state
 
-    const filterHandler =(event)=>{
+
+
+    const filterTempHandler =(event)=>{
         let newDogs=[]
-        const filterTemp = event.target.value 
-            dogs.forEach((dog)=>{
-                let temperament = []
-                if(dog.temperament)temperament= dog.temperament.split(",")
-                let arraySinEspacios = temperament.map((element)=>element.trim())
-                if(arraySinEspacios.includes(filterTemp)) newDogs.push(dog)
-            })
-            setFilteredDogs(newDogs)
+        const filter = event.target.value 
+        dogs.forEach((dog)=>{
+            let temperament = []
+            if(dog.temperament)temperament= dog.temperament.split(",")
+            let arraySinEspacios = temperament.map((element)=>element.trim())
+            if(arraySinEspacios.includes(filter)) newDogs.push(dog)
+        })
+        setFilteredDogs(newDogs)
     }
 
-    const [filteredDogs, setFilteredDogs] = useState(null)
+
+    const sortByName=(event)=>{
+        const orden = event.target.value
+        console.log(orden)
+        let dogsToSort= filteredDogs? [...filteredDogs] : [...dogs]
+        
+        if(orden === "descendente"){
+            dogsToSort.sort((a,b)=>{
+                if(a.name < b.name) return -1;
+                if(a.name > b.name) return 1
+                return 0
+    
+            })
+        }else if("ascendente"){
+            dogsToSort.sort((a,b)=>{
+                if(a.name > b.name) return -1;
+                if(a.name < b.name) return 1
+                return 0
+    
+            })
+        }
+        setFilteredDogs(dogsToSort)
+
+    } 
+
+    const sortByWeight =(event)=>{
+        const orden = event.target.value
+        const dogsToSort= filteredDogs? [...filteredDogs] :[...dogs]
+        const newDogs=[]
+        
+        if(orden === "descendente"){
+            const aux = dogsToSort.map((dog)=>{ 
+                const auxWeight= dog.weight.metric.split(" - ")
+    
+                const weight = Math.max(...auxWeight.map(val =>  parseInt(val)))
+    
+                return {weight:weight ,id:dog.id}})
+                .filter((item)=>!isNaN(item.weight))
+                .sort((a,b)=>b.weight - a.weight)
+    
+                aux.forEach((element)=>{
+                    newDogs.push(dogsToSort.find((dog)=>dog.id === element.id))
+                })
+                setFilteredDogs(newDogs)
+        }else if(orden === "ascendente"){
+                const aux = dogsToSort.map((dog)=>{ 
+                const auxWeight= dog.weight.metric.split(" - ")
+    
+                const weight = Math.max(...auxWeight.map(val =>  parseInt(val)))
+    
+                return {weight:weight ,id:dog.id}})
+                .filter((item)=>!isNaN(item.weight))
+                .sort((a,b)=>a.weight - b.weight)
+    
+                aux.forEach((element)=>{
+                    newDogs.push(dogsToSort.find((dog)=>dog.id === element.id))
+                })
+                setFilteredDogs(newDogs)
+        }
+
+        
+        console.log(newDogs)
+
+    }
+
+
+
     const dogsToRender = filteredDogs || dogs
     let arrayLength = dogsToRender.length
     
@@ -39,7 +109,12 @@ const Home = ({clickHandler})=>{
 
     return(
         <div className="home-container">
-            <Filters temperaments={temperaments} filterHandler={filterHandler}/>
+            <Filters 
+            temperaments={temperaments} 
+            sortByName={sortByName} 
+            filterTempHandler={filterTempHandler}
+            sortByWeight={sortByWeight}
+            />
             { 
                 <Cards
                 dogs={dogsToRender}
