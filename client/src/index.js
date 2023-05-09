02c91d76c9beef66374b189ffc59eb1d3b50d0d3
JesusEGomez <jesus.emanuel.gomez77@gmail.com>
2023-05-08 23:28:21 -0300

    antes de deploying

--- client/src/index.js
+++ client/src/index.js
@@ -7,8 +7,7 @@ import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from "react-router-dom"
 import { Provider } from 'react-redux';
 import store from "./Redux/store"
-import axios from 'axios';
-// axios.defaults.baseURL= ""
+
 
 ReactDOM.render(
   <Provider store={store}>
